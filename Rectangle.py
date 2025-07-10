d5b80a73197e7960c828dd248493e0f22e2d83ff
sheba-allen <shebaallenlalu18@gmail.com>
2025-07-10 14:38:00 +0530

    Add files via upload

---
+++ Rectangle.py
@@ -0,0 +1,6 @@
+l=int(input("Enter length of Rectangle"))
+b=int(input("Enter breadth of Rectangle"))
+Area=l*b
+Perimeter=2*l+b
+print("Area of Rectangle",Area)
+print("Perimeter of Rectangle",Perimeter)
